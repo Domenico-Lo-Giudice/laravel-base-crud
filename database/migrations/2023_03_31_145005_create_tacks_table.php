48aff1cef8dd9522d3d1518938fb3b57a2b03bfb
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-03-31 17:03:07 +0200

    Create Tracks & Migrate, Seeder

---
+++ database/migrations/2023_03_31_145005_create_tacks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('track', function (Blueprint $table) {
+            $table->id();
+            $table->text('title')->unique();
+            $table->string('album', 100);
+            $table->string('author', 80);
+            $table->string('editor', 80);
+            $table->integer('length');
+            $table->text('poster')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tacks');
+    }
+};
