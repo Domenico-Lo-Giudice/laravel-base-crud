a6b2d20b13d1d68db4941ce7110722810e70295b
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-04-03 16:15:06 +0200

    Add home and logo home

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="#">{{ env('APP_NAME')}}</a>
+        <a class="navbar-brand" href="{{ route('homepage') }}">{{ env('APP_NAME')}}</a>
         <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
             aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
