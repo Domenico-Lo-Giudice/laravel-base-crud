f8ebe735ef532728ebff25b9b5f810902d35b762
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-04-03 15:39:00 +0200

    Add resource

--- app/Models/Track.php
+++ app/Models/Track.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Track extends Model
 {
     use HasFactory;
+
+    public function getAbstract($char = 30) {
+        return substr($this->album, 0, $chars);
+
+    }
 }
